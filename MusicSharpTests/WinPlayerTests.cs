42bcc7808bc7f3562184c1dc373ad912cedc1709
Mark-James McDougall <contact@markjames.dev>
2023-12-03 23:07:54 -0500

    Removed unused test

--- MusicSharpTests/WinPlayerTests.cs
+++ MusicSharpTests/WinPlayerTests.cs
@@ -5,6 +5,16 @@ namespace MusicSharpTests;
 public class WinPlayerTests
 {
     [Test]
+    public void Play_NullFile()
+    {
+        // arrange
+        var isFileValid = File.Exists("thisisafail.exe");
+
+        // act and assert
+        Assert.That(isFileValid, Is.False);
+    }
+    
+    [Test]
     public void PlayFromPlaylist_NullFile()
     {
         // arrange
@@ -12,15 +22,5 @@ public class WinPlayerTests
 
         // act and assert
         Assert.Throws<NullReferenceException>(() => player.PlayFromPlaylist(null));
-    }
-
-    [Test]
-    public void OpenStream_NullFile()
-    {
-        // arrange
-        var player = new WinPlayer();
-
-        // act and assert
-        Assert.Throws<NullReferenceException>(() => player.OpenStream(null));
     }
 }

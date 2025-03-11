c38682184bb69d1de4fbe54ce6511930fd685680
Mark-James McDougall <contact@markjames.dev>
2025-03-10 21:25:05 -0400

    Update tests.

--- MusicSharpTests/SoundEngineTests.cs
+++ MusicSharpTests/SoundEngineTests.cs
@@ -1,7 +1,3 @@
-using MusicSharp.SoundEngines;
-using SoundFlow.Backends.MiniAudio;
-using SoundFlow.Enums;
-
 namespace MusicSharpTests;
 
 public class SoundEngineTests
@@ -14,16 +10,5 @@ public class SoundEngineTests
 
         // act and assert
         Assert.That(isFileValid, Is.False);
-    }
-    
-    [Test]
-    public void PlayFromPlaylist_NullFile()
-    {
-        // arrange
-        using var soundEngine = new MiniAudioEngine(44100, Capability.Playback);
-        using var player = new SoundEngine(soundEngine);
-
-        // act and assert
-        Assert.Throws<NullReferenceException>(() => player.Play(null));
     }
 }

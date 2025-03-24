2a733f280bc005a293d5da40dd5370569a1e2090
Mark-James McDougall <contact@markjames.dev>
2025-03-23 20:24:39 -0400

    Remove unnecessary using directive.

--- src/UI/Tui.cs
+++ src/UI/Tui.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Text;
-using Microsoft.Extensions.Primitives;
 using MusicSharp.AudioPlayer;
 using MusicSharp.Enums;
 using MusicSharp.PlaylistHandlers;

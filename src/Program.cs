3123386f64183e2af94ca99d28a4083db82140c0
Mark-James McDougall <contact@markjames.dev>
2025-07-30 00:37:29 -0400

    Remove unnecessary Trim warning suppression.

--- src/Program.cs
+++ src/Program.cs
@@ -8,13 +8,6 @@ namespace MusicSharp;
 
 public static class Program
 {
-    [UnconditionalSuppressMessage("AssemblyLoadTrimming", 
-                                  "IL2104:Assembly 'SoundFlow' produced trim warnings.", 
-                                  Justification = """
-                                                  Can safely ignore as this is the only warning and the 
-                                                  method is only ever called with primitive types
-                                                  https://github.com/LSXPrime/SoundFlow/blob/master/Src/Utils/Extensions.cs#L49
-                                                  """)]
     public static void Main()
     {
         using var audioEngine = new MiniAudioEngine();
